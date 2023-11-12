eaacf9f5608d8de2e7caffbb340365116404bb6b
Corinne <CorinneKogut@gmail.com>
2023-11-12 17:32:29 -0500

    Working on excercise 3

---
+++ temperature_utils_V2.py
@@ -0,0 +1,66 @@
+from typing import Iterable, Tuple
+
+
+def convert_to_celsius(input_temp: float, input_unit_of_measurement: str)-> float:
+    """
+    Given a float representing a temperature in fahrenheit or kelvin, return the corresponding value in celsius.
+
+    :param input_temp: A float representing a temperature in fahrenheit or kelvin
+    :return: A float representing the corresponding value of the input_temp parameter in celsius
+    """
+    if input_unit_of_measurement == 'f':
+        celsius = (input_temp - 32) * 5 / 9
+        return round(celsius, 2)
+    elif input_unit_of_measurement == 'k':
+        celsius = input_temp - 273.15
+        return round(celsius, 2)
+    else:
+        print('Invalid input')
+
+
+def convert_to_fahrenheit(input_temp: float, input_unit_of_measurement: str) -> int:
+    """
+    Given a float representing a temperature in celsius or kelvin, return the corresponding value in fahrenheit.
+
+    :param input_temp: A float representing a temperature in celsius or kelvin
+    :return:  A float representing the corresponding value of the input_temp parameter in fahrenheit
+    """
+    if input_unit_of_measurement == 'f':
+        fahrenheit = (input_temp * 9 / 5) + 32
+        return int(fahrenheit)
+    elif input_unit_of_measurement == 'k':
+        fahrenheit = (input_temp - 273.15) * (9/5) + 32
+        return int(fahrenheit)
+    else:
+        print('Invalid input')
+
+
+def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) -> Tuple[Tuple[float, float]]:
+    """
+    Given a tuple or a list of temperatures, this function returns a tuple of tuples.
+    Each tuple contains two values. The first is the value of the temperatures parameter. The second is the value of
+    the first converted to the unit of measurement specified in the input_unit_of_measurement parameter.
+
+    :param temperatures: An iterable containing temperatures
+    :param input_unit_of_measurement: The unit a measure to use to convert the values in the temperatures parameter
+    :return: A tuple of tuples
+    """
+
+    # Create two lists
+    orig_temp = []
+    converted = []
+
+    # Loop through tuple and assign value to orig_temp
+    for i in temperatures:
+        orig_temp.append(i)
+        # Bases on input_of_measurement, convert and assign to converted
+        if input_unit_of_measurement == 'c':
+            converted.append(convert_to_fahrenheit(i))
+        elif input_unit_of_measurement == 'f':
+            converted.append(convert_to_celsius(i))
+        else:
+            pass
+
+    # Zip the two together
+    return tuple(zip(orig_temp, converted))
+
---
+++ test_temperature_utils_V2.py
@@ -0,0 +1,74 @@
+import unittest
+import temperature_utils_V2
+
+
+class TemperatureUtilsTest(unittest.TestCase):
+
+    def test_convert_fahrenheit_to_celsius(self):
+        test_cases = [
+            (32, 0),
+            (68, 20),
+            (100, 37.78),
+            (104, 40)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_V2.convert_to_celsius(temp_in, 'f'))
+
+    def test_convert_kelvin_to_celsius(self):
+        test_cases = [
+            (0, -273.15),
+            (30, -243.15),
+            (60, -213.15),
+            (100, -173.15)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_V2.convert_to_celsius(temp_in, 'k'))
+
+    def test_convert_invalid_to_celsius(self):
+        test_cases = [
+            (0, None),
+            (30, None),
+            (60, None),
+            (100, None)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_V2.convert_to_celsius(temp_in, 'b'))
+
+    def test_convert_celsius_to_fahrenheit(self):
+        test_cases = [
+            (-17.7778, 0),
+            (0, 32),
+            (100, 212)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_V2.convert_to_fahrenheit(temp_in, 'f'))
+
+    def test_convert_kelvin_to_fahrenheit(self):
+        test_cases = [
+            (0, -459),
+            (300, 80),
+            (350, 170)
+        ]
+        for temp_in, expected in test_cases:
+            with self.subTest(f"{temp_in} -> {expected}"):
+                self.assertEqual(expected, temperature_utils_V2.convert_to_fahrenheit(temp_in, 'k'))
+
+    def test_temperature_tuple(self):
+        temps_input = (32, 68, 100, 104)
+        expected = ((32, 0.0), (68, 20.0), (100, 37.78), (104, 40.0))
+        actual = temperature_utils_V2.temperature_tuple(temps_input, "f")
+        self.assertEqual(expected, actual)
+
+    def test2_temperature_tuple(self):
+        temps_input = (-17.7778, 0, 100)
+        expected = ((-17.7778, 0.0), (0, 32.0), (100, 212.0))
+        actual = temperature_utils_V2.temperature_tuple(temps_input, "c")
+        self.assertEqual(expected, actual)
+
+    def test3_temperature_tuple(self):
+        temps_input = (1, 2, 3)
+        self.assertEqual(tuple(), temperature_utils_V2.temperature_tuple(temps_input, "a"))
